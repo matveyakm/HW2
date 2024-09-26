0f8fabdc26ef5ec761b1228c6f8e8df2882aed87
matveyakm <102146670+matveyakm@users.noreply.github.com>
2024-09-26 16:51:08 +0300

    typo

--- 1909241.c
+++ 1909241.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
-#define ARRAY_LENTH(array) (sizeof(array) / sizeof((array)[0]))
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof((array)[0]))
 #define RUNTIME(start, end) (((double)(end - start)) / CLOCKS_PER_SEC)
 
 // Написать программу, которая заполняет массив случайными значениями (с использованием функции rand из stdlib.h), потом преобразует его без использования дополнительных массивов так, что в начале массива будут элементы, меньшие первого, а в конце — большие либо равные первому. Программа должна работать за линейное время.
@@ -13,11 +13,11 @@ void swap(int *array,int i, int j) {
     array[j] = temp;
 }
 
-void firstToMid(int *randomArray, int arrayLenth) {
+void firstToMid(int *randomArray, int arrayLength) {
     int countOfNumsThatBiggerThanFirst = 0;
     int firstElementValue = randomArray[0];
     int i = 1;
-    while (i < arrayLenth) {
+    while (i < arrayLength) {
         if (randomArray[i] >= firstElementValue) {
             countOfNumsThatBiggerThanFirst++;
         }
@@ -32,28 +32,28 @@ void firstToMid(int *randomArray, int arrayLenth) {
     }
 }
 
-void randomizeArray(int *array, int arrayLenth, int seed) {
+void randomizeArray(int *array, int arrayLength, int seed) {
     if (seed == 0) {
         srand(time(NULL));
     } else {
         srand(seed);
     }
-    for (int i = 0; i < arrayLenth; i++) {
+    for (int i = 0; i < arrayLength; i++) {
         array[i] = rand() % 1000;
     }
 }
 
-void printArray(int *array, int arrayLenth) {
-    for (int i = 0; i < arrayLenth; i++) { 
+void printArray(int *array, int arrayLength) {
+    for (int i = 0; i < arrayLength; i++) { 
         printf("%d ",array[i]);
     }
     printf("\n");
 }
 
-const bool resultTest(int *array, int arrayLenth, int firstElement) {
+const bool resultTest(int *array, int arrayLength, int firstElement) {
     bool afterFirstElement = false;
     bool isTestSuccesful = true;
-    for (int i = 0; i < arrayLenth; i++) { 
+    for (int i = 0; i < arrayLength; i++) { 
         if (array[i] == firstElement) {
             afterFirstElement = true;
         }
@@ -64,18 +64,18 @@ const bool resultTest(int *array, int arrayLenth, int firstElement) {
     return isTestSuccesful;
 }
 
-const bool globalTest(int lenthOfTestingArray) {
+const bool globalTest(int lengthOfTestingArray) {
     int seed[] = {52, 666, 4, 89, 5843, 101, 123, 5, 54358, 33, 947};
-    int *arrayNeedToTest = malloc(lenthOfTestingArray * sizeof(int));
+    int *arrayNeedToTest = malloc(lengthOfTestingArray * sizeof(int));
     bool isTestSuccesful = true;
     int countOfSuccesfulTests = 0;
     clock_t start, end;
     start = clock();
-    for (int seedPeaker = 0; seedPeaker < ARRAY_LENTH(seed); seedPeaker++) { 
-        randomizeArray(arrayNeedToTest, lenthOfTestingArray, seed[seedPeaker]);
+    for (int seedPeaker = 0; seedPeaker < ARRAY_LENGTH(seed); seedPeaker++) { 
+        randomizeArray(arrayNeedToTest, lengthOfTestingArray, seed[seedPeaker]);
         int firstElement = arrayNeedToTest[0];
-        firstToMid(arrayNeedToTest, lenthOfTestingArray);
-        bool localTestSuccesful = resultTest(arrayNeedToTest, lenthOfTestingArray, firstElement);
+        firstToMid(arrayNeedToTest, lengthOfTestingArray);
+        bool localTestSuccesful = resultTest(arrayNeedToTest, lengthOfTestingArray, firstElement);
         if (!localTestSuccesful) {
             printf("Test with seed [%d] failed.\n", seed[seedPeaker]);
         } else {
@@ -85,23 +85,23 @@ const bool globalTest(int lenthOfTestingArray) {
     }
     end = clock();
     printf(isTestSuccesful ? "Test succesful " : " Test failed ");
-    printf("[%d/%d tests succesful with lenth %d | %.5f secs]", countOfSuccesfulTests, ARRAY_LENTH(seed), lenthOfTestingArray, RUNTIME(start, end));
+    printf("[%d/%d tests succesful with length %d | %.5f secs]", countOfSuccesfulTests, ARRAY_LENGTH(seed), lengthOfTestingArray, RUNTIME(start, end));
     free(arrayNeedToTest);
     return isTestSuccesful;
 }
 
 int main()
 {
-    int randomArrayLenth = 8;
-    int *randomArray = malloc(randomArrayLenth * sizeof(int));
+    int randomArrayLength = 8;
+    int *randomArray = malloc(randomArrayLength * sizeof(int));
 
-    randomizeArray(randomArray, randomArrayLenth, 0);
+    randomizeArray(randomArray, randomArrayLength, 0);
     int firstElemenent = randomArray[0];
-    printArray(randomArray, randomArrayLenth);
+    printArray(randomArray, randomArrayLength);
 
-    firstToMid(randomArray, randomArrayLenth);
-    printArray(randomArray, randomArrayLenth);
-    printf(resultTest(randomArray, randomArrayLenth, firstElemenent) ? "Local test (for random nums) succesful\n" : "Local test (for random nums) failed\n");
+    firstToMid(randomArray, randomArrayLength);
+    printArray(randomArray, randomArrayLength);
+    printf(resultTest(randomArray, randomArrayLength, firstElemenent) ? "Local test (for random nums) succesful\n" : "Local test (for random nums) failed\n");
     free(randomArray);
 
     globalTest(10000);
